bc641ddd21cb8f3b1ad824cf01747b5dc37de4f2
apstndb <803393+apstndb@users.noreply.github.com>
2026-08-04 22:45:07 +0900

    Bump Cloud Spanner emulator to 1.5.56

--- spanemuboost.go
+++ spanemuboost.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	DefaultEmulatorImage = "gcr.io/cloud-spanner-emulator/emulator:1.5.55"
+	DefaultEmulatorImage = "gcr.io/cloud-spanner-emulator/emulator:1.5.56"
 	DefaultProjectID     = "emulator-project"
 	DefaultInstanceID    = "emulator-instance"
 	DefaultDatabaseID    = "emulator-database"
